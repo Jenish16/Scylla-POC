61653c274e791a9427984f975f16cac4bb33d27b
Jenish <22037861+Jenish16@users.noreply.github.com>
2024-06-22 13:27:24 +0530

    Initial Commit

---
+++ src/main/java/com/codeistari/scyllapoc/ScyllaPocApplication.java
@@ -0,0 +1,13 @@
+package com.codeistari.scyllapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScyllaPocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ScyllaPocApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/codeistari/scyllapoc/ScyllaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeistari.scyllapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScyllaPocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
